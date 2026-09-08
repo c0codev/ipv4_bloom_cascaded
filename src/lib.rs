fb457037e9f5437b48c9d94c4945f42d39eb882b
c0codev <lunita07gp@gmail.com>
2026-09-08 22:50:34 +0000

    Modules of the whole project
    Library of the project (supports both no_std mode and std mode)

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(feature = "baremetal", no_main)]
+
+//Main module (always compile either std or no_std/baremetal)
+pub mod cascaded_filter;
+
+
+//Benchmarking modules (std_only)
+#[cfg(feature = "std")]
+pub mod bloomfilter_wrapper;
+#[cfg(feature = "std")]
+pub mod cuckoofilter_wrapper;
+#[cfg(feature = "std")]
+pub mod xorf_wrapper;
+#[cfg(feature = "std")]
+pub mod flat_filter;
+#[cfg(feature = "std")]
+pub mod fastbloom_wrapper;
