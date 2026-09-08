6fe7909c318384299b0f36467f5122232e62858e
c0codev <lunita07gp@gmail.com>
2026-09-08 22:48:19 +0000

    A wrapper of fastbloom crate (used exclusively for benchmarks)
    Another wrapper to allow public auditing of the wrappers used on the benchmarks for transparency reasons.
    
    Uses fastbloom crate, found at https://crates.io/crates/fastbloom

---
+++ src/fastbloom_wrapper.rs
@@ -0,0 +1,27 @@
+use fastbloom::AtomicBloomFilter;
+
+pub struct ThirdPartyFastBloom {
+    inner: AtomicBloomFilter,
+}
+
+impl ThirdPartyFastBloom {
+    pub fn new(_malicious_count: usize) -> Self {
+        Self {
+            inner: AtomicBloomFilter::with_num_bits(75_628_544).hashes(7),
+        }
+    }
+
+    pub fn inject_ban(&self, ip: u32) {
+        self.inner.insert(&ip);
+    }
+
+    #[inline(always)]
+    pub fn batch_processing(&self, ips: [u32; 16]) -> [u32; 16] {
+        let mut results = [0u32; 16];
+        for i in 0..16 {
+            //1 = Pass 0 = Banned
+            results[i] = if self.inner.contains(&ips[i]) { 0 } else { 1 };
+        }
+        results
+    }
+}
