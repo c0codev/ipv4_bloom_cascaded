15c47c62e1f17a5f5ff20a5a7a8082b44772cc29
c0codev <personal@c0co.dev>
2026-09-11 21:40:47 +0000

    Minor Update
    Updated the comment to include emojis

--- src/fastbloom_wrapper.rs
+++ src/fastbloom_wrapper.rs
@@ -19,7 +19,7 @@ impl ThirdPartyFastBloom {
     pub fn batch_processing(&self, ips: [u32; 16]) -> [u32; 16] {
         let mut results = [0u32; 16];
         for i in 0..16 {
-            //1 = Pass 0 = Banned
+            // --- ✅ 1 = Pass 0 = Banned ❌ ---
             results[i] = if self.inner.contains(&ips[i]) { 0 } else { 1 };
         }
         results
